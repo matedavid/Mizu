f225c12d3079fc42484d6a947a45e9fccaa23b00
matedavid <david.mate.lopez@gmail.com>
2024-06-16 20:49:30 +0200

    C++ things

--- Mizu/src/renderer/camera.h
+++ Mizu/src/renderer/camera.h
@@ -19,8 +19,8 @@ class Camera {
   protected:
     glm::mat4 m_view{};
 
-    glm::vec3 m_position;
-    glm::vec3 m_rotation;
+    glm::vec3 m_position{};
+    glm::vec3 m_rotation{};
 
     void recalculate_view_matrix();
 };
