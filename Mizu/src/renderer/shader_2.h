d17fd8c54dfc60ce3e2aa61f8a48aea84dda2f91
matedavid <david.mate.lopez@gmail.com>
2024-07-08 22:41:04 +0200

    some fixes

--- Mizu/src/renderer/shader_2.h
+++ Mizu/src/renderer/shader_2.h
@@ -24,35 +24,35 @@ using members_vec_t = std::vector<Shader2MemberInfo>;
         struct _first_member_id {};                                                                      \
         typedef _func (*_member_func)(_first_member_id, Mizu::members_vec_t&, const Parameters& params); \
         static _func _append_member_get_prev_func(_first_member_id,                                      \
-                                                  Mizu::members_vec_t& members,                          \
-                                                  const Parameters& params) {                            \
+                                                  [[maybe_unused]] Mizu::members_vec_t& members,         \
+                                                  [[maybe_unused]] const Parameters& params) {           \
             return nullptr;                                                                              \
         }                                                                                                \
         typedef _first_member_id
 
-#define SHADER_PARAMETER_RG_TEXTURE2D(name)                                             \
-    _member_##name;                                                                     \
-                                                                                        \
-  public:                                                                               \
-    Mizu::RGTextureRef name;                                                            \
-                                                                                        \
-  private:                                                                              \
-    struct _next_member_##name {};                                                      \
-    static _func _append_member_get_prev_func(                                          \
-        _next_member_##name, Mizu::members_vec_t& members, const Parameters& params) {  \
-        auto info = Mizu::Shader2MemberInfo{.mem_name = #name, .value = params.##name}; \
-        members.push_back(info);                                                        \
-        _func (*prev_func)(_member_##name, Mizu::members_vec_t&, const Parameters&);    \
-        prev_func = _append_member_get_prev_func;                                       \
-        return (void*)prev_func;                                                        \
-    }                                                                                   \
+#define SHADER_PARAMETER_RG_TEXTURE2D(name)                                            \
+    _member_##name;                                                                    \
+                                                                                       \
+  public:                                                                              \
+    Mizu::RGTextureRef name;                                                           \
+                                                                                       \
+  private:                                                                             \
+    struct _next_member_##name {};                                                     \
+    static _func _append_member_get_prev_func(                                         \
+        _next_member_##name, Mizu::members_vec_t& members, const Parameters& params) { \
+        auto info = Mizu::Shader2MemberInfo{.mem_name = #name, .value = params.name};  \
+        members.push_back(info);                                                       \
+        _func (*prev_func)(_member_##name, Mizu::members_vec_t&, const Parameters&);   \
+        prev_func = _append_member_get_prev_func;                                      \
+        return (void*)prev_func;                                                       \
+    }                                                                                  \
     typedef _next_member_##name
 
 #define END_SHADER_PARAMETERS()                                                             \
     _last_member_id;                                                                        \
                                                                                             \
   public:                                                                                   \
-    static Mizu::members_vec_t get_members(const Parameters& params = {}) {                      \
+    static Mizu::members_vec_t get_members(const Parameters& params = {}) {                 \
         Mizu::members_vec_t members = Parent::Parameters::get_members(params);              \
         _func (*last_func)(_last_member_id, Mizu::members_vec_t&, const Parameters&);       \
         last_func = _append_member_get_prev_func;                                           \
@@ -68,7 +68,7 @@ using members_vec_t = std::vector<Shader2MemberInfo>;
 class BaseShader final {
   public:
     struct Parameters {
-        static Mizu::members_vec_t get_members(const Parameters& params = {}) { return {}; }
+        static Mizu::members_vec_t get_members([[maybe_unused]] const Parameters& params = {}) { return {}; }
     };
 };
 
