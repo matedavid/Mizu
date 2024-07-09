f426e7e0799b36aa1c04a61d51c1d9146bb35d36
matedavid <david.mate.lopez@gmail.com>
2024-07-09 18:45:00 +0200

    Add Shader declaration member type as enum

--- Mizu/src/renderer/shader_declaration.h
+++ Mizu/src/renderer/shader_declaration.h
@@ -12,12 +12,17 @@ namespace Mizu {
 
 using ShaderDeclarationMemberT = std::variant<RGTextureRef>;
 
-struct Shader2MemberInfo {
+enum class ShaderDeclarationMemberType {
+    RGTexture2D,
+};
+
+struct ShaderDeclarationMemberInfo {
     std::string mem_name;
+    ShaderDeclarationMemberType mem_type;
     ShaderDeclarationMemberT value;
 };
 
-using members_vec_t = std::vector<Shader2MemberInfo>;
+using members_vec_t = std::vector<ShaderDeclarationMemberInfo>;
 
 #define BEGIN_SHADER_PARAMETERS()                                                                        \
     class Parameters : public Parent::Parameters {                                                       \
@@ -52,26 +57,27 @@ using members_vec_t = std::vector<Shader2MemberInfo>;
     }                                                                                       \
     ;
 
-#define SHADER_PARAMETER_IMPL(name, type, default_value, type_enum)                    \
-    _member_##name;                                                                    \
-                                                                                       \
-  public:                                                                              \
-    type name = default_value;                                                         \
-                                                                                       \
-  private:                                                                             \
-    struct _next_member_##name {};                                                     \
-    static _func _append_member_get_prev_func(                                         \
-        _next_member_##name, Mizu::members_vec_t& members, const Parameters& params) { \
-        auto info = Mizu::Shader2MemberInfo{.mem_name = #name, .value = params.name};  \
-        members.push_back(info);                                                       \
-        _func (*prev_func)(_member_##name, Mizu::members_vec_t&, const Parameters&);   \
-        prev_func = _append_member_get_prev_func;                                      \
-        return (void*)prev_func;                                                       \
-    }                                                                                  \
+#define SHADER_PARAMETER_IMPL(name, type, default_value, type_enum)                                                    \
+    _member_##name;                                                                                                    \
+                                                                                                                       \
+  public:                                                                                                              \
+    type name = default_value;                                                                                         \
+                                                                                                                       \
+  private:                                                                                                             \
+    struct _next_member_##name {};                                                                                     \
+    static _func _append_member_get_prev_func(                                                                         \
+        _next_member_##name, Mizu::members_vec_t& members, const Parameters& params) {                                 \
+        auto info = Mizu::ShaderDeclarationMemberInfo{.mem_name = #name, .mem_type = type_enum, .value = params.name}; \
+        members.push_back(info);                                                                                       \
+        _func (*prev_func)(_member_##name, Mizu::members_vec_t&, const Parameters&);                                   \
+        prev_func = _append_member_get_prev_func;                                                                      \
+        return (void*)prev_func;                                                                                       \
+    }                                                                                                                  \
     typedef _next_member_##name
 
 #define SHADER_PARAMETER_RG_TEXTURE2D(name) \
-    SHADER_PARAMETER_IMPL(name, Mizu::RGTextureRef, Mizu::RGTextureRef::invalid(), )
+    SHADER_PARAMETER_IMPL(                  \
+        name, Mizu::RGTextureRef, Mizu::RGTextureRef::invalid(), Mizu::ShaderDeclarationMemberType::RGTexture2D)
 
 #define IMPLEMENT_SHADER(vertex_path, fragment_path)                        \
     static std::shared_ptr<Mizu::GraphicsShader> get_shader() {             \
@@ -90,7 +96,6 @@ class ShaderDeclaration {
   public:
     static std::shared_ptr<GraphicsShader> get_shader() { return nullptr; }
 
-  protected:
     using Parent = T;
 };
 
