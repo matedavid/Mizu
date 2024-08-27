ef0d27f8efcb2266635864fb7d12b5336dc119b0
David Mate <david.mate.lopez@gmail.com>
2024-08-27 17:17:56 +0200

    First person camera movement follows orientation

--- Mizu/src/plugins/camera_controllers/first_person_camera_controller.cpp
+++ Mizu/src/plugins/camera_controllers/first_person_camera_controller.cpp
@@ -18,26 +18,6 @@ void FirstPersonCameraController::set_config(Config config) {
 void FirstPersonCameraController::update(double ts) {
     const auto fts = static_cast<float>(ts);
 
-    // Position
-    if (m_config.move_modifier_key == ModifierKeyBits::None
-        || Input::is_modifier_keys_pressed(m_config.move_modifier_key)) {
-        glm::vec3 movement(0.0f);
-
-        if (Input::is_key_pressed(Key::W)) {
-            movement += m_config.longitudinal_movement_speed * fts * glm::vec3(0.0f, 0.0f, -1.0f);
-        } else if (Input::is_key_pressed(Key::S)) {
-            movement += m_config.longitudinal_movement_speed * fts * glm::vec3(0.0f, 0.0f, 1.0f);
-        }
-
-        if (Input::is_key_pressed(Key::A)) {
-            movement += m_config.lateral_movement_speed * fts * glm::vec3(-1.0f, 0.0f, 0.0f);
-        } else if (Input::is_key_pressed(Key::D)) {
-            movement += m_config.lateral_movement_speed * fts * glm::vec3(1.0f, 0.0f, 0.0f);
-        }
-
-        set_position(m_position + movement);
-    }
-
     // Rotation
     if (m_config.rotate_modifier_key == ModifierKeyBits::None
         || Input::is_modifier_keys_pressed(m_config.rotate_modifier_key)) {
@@ -49,6 +29,30 @@ void FirstPersonCameraController::update(double ts) {
 
         const glm::vec3 new_rotation = m_rotation + glm::vec3(-yaw, pitch, 0.0f);
         set_rotation(new_rotation);
+    }
+
+    // Position
+    if (m_config.move_modifier_key == ModifierKeyBits::None
+        || Input::is_modifier_keys_pressed(m_config.move_modifier_key)) {
+        glm::vec3 movement(0.0f);
+
+        const auto front = glm::normalize(glm::vec3(m_view[0][2], m_view[1][2], m_view[2][2]));
+        const auto right = glm::normalize(glm::vec3(m_view[0][0], m_view[1][0], m_view[2][0]));
+
+        if (Input::is_key_pressed(Key::W)) {
+            // z+ is towards camera, so we need -front to move forward
+            movement += m_config.longitudinal_movement_speed * fts * (-front);
+        } else if (Input::is_key_pressed(Key::S)) {
+            movement += m_config.longitudinal_movement_speed * fts * front;
+        }
+
+        if (Input::is_key_pressed(Key::A)) {
+            movement += m_config.lateral_movement_speed * fts * (-right);
+        } else if (Input::is_key_pressed(Key::D)) {
+            movement += m_config.lateral_movement_speed * fts * right;
+        }
+
+        set_position(m_position + movement);
     }
 }
 
