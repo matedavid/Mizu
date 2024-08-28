92a424ede3d755c95281f5f0b126be4ede09b2c7
David Mate <david.mate.lopez@gmail.com>
2024-08-28 11:48:26 +0200

    MizuExample updated to use FirstPersonCameraController

--- apps/example/main.cpp
+++ apps/example/main.cpp
@@ -1,5 +1,7 @@
 #include "Mizu/Mizu.h"
 
+#include "plugins/camera_controllers/first_person_camera_controller.h"
+
 constexpr uint32_t WIDTH = 1920;
 constexpr uint32_t HEIGHT = 1080;
 
@@ -7,8 +9,14 @@ class ExampleLayer : public Mizu::Layer {
   public:
     ExampleLayer() {
         const float aspect_ratio = static_cast<float>(WIDTH) / static_cast<float>(HEIGHT);
-        m_camera = std::make_shared<Mizu::PerspectiveCamera>(glm::radians(60.0f), aspect_ratio, 0.001f, 100.0f);
-        m_camera->set_position({0.0f, 0.0f, 1.0f});
+        // m_camera = std::make_shared<Mizu::PerspectiveCamera>(glm::radians(60.0f), aspect_ratio, 0.001f, 100.0f);
+        // m_camera->set_position({0.0f, 0.0f, 1.0f});
+
+        m_camera_controller = std::make_unique<Mizu::FirstPersonCameraController>(glm::radians(60.0f), aspect_ratio, 0.001f, 100.0f);
+        m_camera_controller->set_position({0.0f, 0.0f, 1.0f});
+        m_camera_controller->set_config(Mizu::FirstPersonCameraController::Config{
+            .rotate_modifier_key = Mizu::ModifierKeyBits::Shift,
+        });
 
         m_scene = std::make_shared<Mizu::Scene>("Example Scene");
 
@@ -23,7 +31,8 @@ class ExampleLayer : public Mizu::Layer {
     }
 
     void on_update(double ts) override {
-        m_renderer->render(*m_camera);
+        m_camera_controller->update(ts);
+        m_renderer->render(*m_camera_controller);
         m_presenter->present();
     }
 
@@ -31,10 +40,12 @@ class ExampleLayer : public Mizu::Layer {
         Mizu::Renderer::wait_idle();
         init(event.get_width(), event.get_height());
         m_presenter->texture_changed(m_renderer->output_texture());
-        m_camera->set_aspect_ratio(static_cast<float>(WIDTH) / static_cast<float>(HEIGHT));
+        // m_camera->set_aspect_ratio(static_cast<float>(WIDTH) / static_cast<float>(HEIGHT));
+        m_camera_controller->set_aspect_ratio(static_cast<float>(WIDTH) / static_cast<float>(HEIGHT));
     }
 
     void on_key_pressed(Mizu::KeyPressedEvent& event) override {
+        /*
         glm::vec3 pos = m_camera->get_position();
 
         if (event.get_key() == Mizu::Key::S) {
@@ -62,11 +73,13 @@ class ExampleLayer : public Mizu::Layer {
         }
 
         m_camera->set_position(pos);
+        */
     }
 
   private:
     std::shared_ptr<Mizu::Scene> m_scene;
     std::shared_ptr<Mizu::PerspectiveCamera> m_camera;
+    std::unique_ptr<Mizu::FirstPersonCameraController> m_camera_controller;
     std::shared_ptr<Mizu::Presenter> m_presenter;
 
     std::unique_ptr<Mizu::ISceneRenderer> m_renderer;
