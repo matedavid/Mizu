b181b448df409e4e401fd6fd1874f5cf55a8778d
David Mate <david.mate.lopez@gmail.com>
2025-02-27 22:58:27 +0100

    Changed RG types so that not all of them inherit from UUID so that I can get some typechecking instead of passing RGBufferRef to RGImageRef :)

--- Mizu/src/render_core/render_graph/render_graph_types.h
+++ Mizu/src/render_core/render_graph/render_graph_types.h
@@ -7,26 +7,42 @@
 #include "render_core/rhi/compute_pipeline.h"
 #include "render_core/rhi/graphics_pipeline.h"
 
-#define CREATE_RG_UUID_TYPE_INHERIT(name, inherit)                  \
-    namespace Mizu                                                  \
-    {                                                               \
-    struct name : public inherit                                    \
-    {                                                               \
-        name() : inherit() {}                                       \
-        name(UUID uuid) : inherit(static_cast<UUID::Type>(uuid)) {} \
-        explicit name(UUID::Type value) : inherit(value) {}         \
-    };                                                              \
-    }                                                               \
-    template <>                                                     \
-    struct std::hash<Mizu::name>                                    \
-    {                                                               \
-        Mizu::UUID::Type operator()(const Mizu::name& k) const      \
-        {                                                           \
-            return static_cast<Mizu::UUID::Type>(k);                \
-        }                                                           \
+#define CREATE_RG_UUID_TYPE(name, optional_include)              \
+    namespace Mizu                                               \
+    {                                                            \
+    struct name optional_include                                 \
+    {                                                            \
+        name() : m_uuid(UUID()) {}                               \
+        name(UUID uuid) : m_uuid(uuid) {}                        \
+        explicit name(UUID::Type value) : m_uuid(UUID(value)) {} \
+        static name invalid()                                    \
+        {                                                        \
+            return UUID::invalid();                              \
+        }                                                        \
+        operator UUID::Type() const                              \
+        {                                                        \
+            return static_cast<UUID::Type>(m_uuid);              \
+        }                                                        \
+        bool operator==(const name& other) const                 \
+        {                                                        \
+            return m_uuid == other.m_uuid;                       \
+        }                                                        \
+                                                                 \
+      private:                                                   \
+        UUID m_uuid;                                             \
+    };                                                           \
+    }                                                            \
+    template <>                                                  \
+    struct std::hash<Mizu::name>                                 \
+    {                                                            \
+        Mizu::UUID::Type operator()(const Mizu::name& k) const   \
+        {                                                        \
+            return static_cast<Mizu::UUID::Type>(k);             \
+        }                                                        \
     }
 
-#define CREATE_RG_UUID_TYPE_BASE(name) CREATE_RG_UUID_TYPE_INHERIT(name, UUID)
+#define CREATE_RG_UUID_TYPE_BASE(name) CREATE_RG_UUID_TYPE(name, )
+#define CREATE_RG_UUID_TYPE_INHERIT(name, inherit) CREATE_RG_UUID_TYPE(name, : public inherit)
 
 namespace Mizu
 {
