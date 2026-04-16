619b6458ac7bdb5b0638a2f548dc0428206e3658
David Mate <david.mate.lopez@gmail.com>
2026-04-16 03:30:57 +0200

    Work stealing dequeue initial implementation + tests

---
+++ Engine/src/core/public/core/job_system/work_stealing_deque.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <atomic>
+#include <vector>
+
+#include "base/debug/assert.h"
+
+namespace Mizu
+{
+
+template <typename T, size_t Capacity>
+class WorkStealingDeque
+{
+    static_assert(Capacity > 0, "Can't create WorkStealingDeque with Capacity == 0");
+    static_assert(std::is_trivially_destructible_v<T>, "T must be trivially destructible");
+
+    static constexpr int64_t IntCapacity = static_cast<int64_t>(Capacity);
+
+  public:
+    WorkStealingDeque()
+    {
+        m_queue.resize(Capacity);
+
+        m_top.store(0);
+        m_bottom.store(0);
+    }
+
+    void push(T value)
+    {
+        const int64_t bottom = m_bottom.load(std::memory_order_relaxed);
+        const int64_t top = m_top.load(std::memory_order_acquire);
+
+        if (bottom - top > IntCapacity - 1)
+        {
+            MIZU_ASSERT(false, "Queue is full");
+            return;
+        }
+
+        m_queue[static_cast<size_t>(bottom % IntCapacity)] = std::move(value);
+
+        std::atomic_thread_fence(std::memory_order_release);
+        m_bottom.store(bottom + 1, std::memory_order_relaxed);
+    }
+
+    bool pop(T& out)
+    {
+        const int64_t bottom = m_bottom.load(std::memory_order_relaxed) - 1;
+        m_bottom.store(bottom, std::memory_order_relaxed);
+
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+
+        int64_t top = m_top.load(std::memory_order_relaxed);
+
+        bool valid = true;
+        if (top <= bottom)
+        {
+            // The queue is not empty
+
+            out = m_queue[static_cast<size_t>(bottom % IntCapacity)];
+            if (top == bottom)
+            {
+                // if top == bottom, we're accessing the last element in the queue. We have to make sure the
+                // syncronization between the stealer thread is mantained
+                if (!m_top.compare_exchange_strong(top, top + 1, std::memory_order_seq_cst, std::memory_order_relaxed))
+                {
+                    // The stealer thread got it first :(
+                    valid = false;
+                }
+
+                m_bottom.store(bottom + 1, std::memory_order_relaxed);
+            }
+        }
+        else
+        {
+            // Empty queue
+
+            m_bottom.store(bottom + 1, std::memory_order_relaxed);
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    bool steal(T& out)
+    {
+        int64_t top = m_top.load(std::memory_order_acquire);
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+
+        const int64_t bottom = m_bottom.load(std::memory_order_acquire);
+
+        bool valid = false;
+        if (top < bottom)
+        {
+            // The queue is not empty
+
+            valid = true;
+            out = m_queue[static_cast<size_t>(top % IntCapacity)];
+
+            if (!m_top.compare_exchange_strong(top, top + 1, std::memory_order_seq_cst, std::memory_order_relaxed))
+            {
+                // Someone modified top before us
+                valid = false;
+            }
+        }
+
+        return valid;
+    }
+
+  private:
+    std::vector<T> m_queue;
+    std::atomic<int64_t> m_top, m_bottom;
+};
+
+} // namespace Mizu
