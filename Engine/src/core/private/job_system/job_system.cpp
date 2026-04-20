978768c2b137972e775a99dbabea37d534119eab
David Mate <david.mate.lopez@gmail.com>
2026-04-20 02:37:09 +0200

    checkpoint for JobSystem2

--- Engine/src/core/private/job_system/job_system.cpp
+++ Engine/src/core/private/job_system/job_system.cpp
@@ -1,7 +1,23 @@
 #include "core/job_system/job_system.h"
 
+#include "base/debug/logging.h"
+
 namespace Mizu
 {
+
+// Needs to be here because it needs to know that JobSystem2 is a complete type to be able to call submit_internal
+JobHandle2 PendingJob::submit()
+{
+    m_submitted = true;
+    return m_owner->submit_internal(std::move(*this));
+}
+
+// Needs to be here because it needs to know that JobSystem2 is a complete type to be able to call submit_internal
+JobHandle2 PendingBatch::submit()
+{
+    m_submitted = true;
+    return m_owner->submit_internal(std::move(*this));
+}
 
 bool JobSystem2::init(uint32_t num_workers)
 {
@@ -10,5 +26,148 @@ bool JobSystem2::init(uint32_t num_workers)
 }
 
 void JobSystem2::worker_job() {}
+
+JobHandle2 JobSystem2::submit_internal(PendingJob&& job)
+{
+    CompletionRecord& completion_record = allocate_completion_record();
+    JobRecord& job_record = allocate_job_record();
+
+    init_completion_record(completion_record, 1);
+    init_job_record(job.m_desc, job_record, completion_record);
+
+    for (const JobHandle2& job_dependency : job.m_dependencies)
+    {
+        CompletionRecord* dep_completion_record = try_get_job_handle_completion_record(job_dependency);
+        if (dep_completion_record == nullptr)
+        {
+            MIZU_LOG_ERROR("Trying to schedule job '{}' with invalid dependency", job.m_desc.m_name);
+            continue;
+        }
+
+        if (dep_completion_record->is_completed.load(std::memory_order_acquire))
+            continue;
+
+        WaitNode& wait_node = allocate_wait_node();
+        wait_node.type = WaitNodeType::DependencyEdge;
+        wait_node.target_job_index = job_record.pool_index;
+
+        if (!try_push_wait_node(*dep_completion_record, wait_node))
+        {
+            // Completion closed, release allocated wait node
+            free_wait_node(wait_node);
+            continue;
+        }
+
+        job_record.num_remaining_dependencies += 1;
+    }
+
+    job_record.state = job_record.num_remaining_dependencies == 0 ? JobState::Ready : JobState::PendingDependencies;
+
+    JobHandle2 handle{};
+    handle.completion_index = completion_record.pool_index;
+    handle.generation = completion_record.generation;
+    handle.owner = this;
+
+    return handle;
+}
+
+JobHandle2 JobSystem2::submit_internal(PendingBatch&& batch)
+{
+    (void)batch;
+    MIZU_UNREACHABLE("Not implemented");
+    return JobHandle2();
+}
+
+void JobSystem2::init_job_record(JobDescription& job, JobRecord& job_record, const CompletionRecord& completion_record)
+{
+    // Can't set state and num_remaining_dependencies here because the dependency jobs may have already finished
+    job_record.state = JobState::PendingDependencies;
+    job_record.func = std::move(job.m_func);
+    job_record.num_remaining_dependencies = 0;
+    job_record.completion_index = completion_record.pool_index;
+}
+
+void JobSystem2::init_completion_record(CompletionRecord& completion_record, uint32_t counter)
+{
+    completion_record.is_completed.store(false, std::memory_order_relaxed);
+    completion_record.counter.store(counter, std::memory_order_relaxed);
+    completion_record.waiting_jobs_head.store(IntrusiveFreeListInvalidIndex, std::memory_order_relaxed);
+    completion_record.references = 1;
+}
+
+CompletionRecord* JobSystem2::try_get_job_handle_completion_record(const JobHandle2& handle)
+{
+    if (!handle.is_valid())
+        return nullptr;
+
+    if (handle.owner != this)
+        return nullptr;
+
+    CompletionRecord& record = m_completion_record_pool.get(handle.completion_index);
+    if (record.generation != handle.generation)
+        return nullptr;
+
+    return &record;
+}
+
+CompletionRecord& JobSystem2::get_job_handle_completion_record(const JobHandle2& handle)
+{
+    CompletionRecord* record = try_get_job_handle_completion_record(handle);
+    MIZU_ASSERT(record != nullptr, "Invalid job handle");
+    return *record;
+}
+
+bool JobSystem2::try_push_wait_node(CompletionRecord& completion_record, WaitNode& wait_node)
+{
+    while (true)
+    {
+        IntrusiveFreeListIndex head = completion_record.waiting_jobs_head.load(std::memory_order_acquire);
+        if (head == CompletionRecord::ClosedWaitingList)
+            return false; // Completion is closed
+
+        wait_node.next = head;
+
+        if (completion_record.waiting_jobs_head.compare_exchange_weak(
+                head, wait_node.pool_index, std::memory_order_acq_rel, std::memory_order_relaxed))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+JobRecord& JobSystem2::allocate_job_record()
+{
+    IntrusiveFreeListIndex index = m_job_record_pool.allocate();
+    MIZU_ASSERT(index != IntrusiveFreeListInvalidIndex, "JobRecord allocate failed");
+
+    return m_job_record_pool.get(index);
+}
+
+CompletionRecord& JobSystem2::allocate_completion_record()
+{
+    IntrusiveFreeListIndex index = m_completion_record_pool.allocate();
+    MIZU_ASSERT(index != IntrusiveFreeListInvalidIndex, "CompletionRecord allocate failed");
+
+    CompletionRecord& record = m_completion_record_pool.get(index);
+    record.generation += 1;
+
+    return record;
+}
+
+WaitNode& JobSystem2::allocate_wait_node()
+{
+    IntrusiveFreeListIndex index = m_wait_node_pool.allocate();
+    MIZU_ASSERT(index != IntrusiveFreeListInvalidIndex, "WaitNode allocate failed");
+
+    return m_wait_node_pool.get(index);
+}
+
+void JobSystem2::free_wait_node(const WaitNode& wait_node)
+{
+    MIZU_ASSERT(wait_node.pool_index != IntrusiveFreeListInvalidIndex, "Trying to free invalid WaitNode");
+    m_wait_node_pool.free(wait_node.pool_index);
+}
 
 } // namespace Mizu
--- Engine/src/core/public/core/job_system/job_system.h
+++ Engine/src/core/public/core/job_system/job_system.h
@@ -1,186 +1,282 @@
 #pragma once
 
-#include <cstddef>
-#include <cstdint>
+#include <atomic>
 #include <limits>
+#include <span>
 #include <string_view>
+#include <utility>
 
 #include "base/containers/inplace_vector.h"
 #include "base/debug/assert.h"
 
+#include "core/job_system/inplace_job_function.h"
+#include "core/job_system/intrusive_free_list.h"
+#include "core/job_system/work_stealing_deque.h"
 #include "mizu_core_module.h"
 
 namespace Mizu
 {
 
-constexpr size_t InplaceJobMemoryBytes = 48;
 constexpr size_t MaxJobDependencies = 8;
 
 enum class JobAffinity
 {
-    None,
+    Any,
     Main,
+    SimulationHigh,
     Simulation,
+    RenderHigh,
     Render,
 };
 
-struct JobHandle
-{
-    static constexpr size_t InvalidIndex = std::numeric_limits<size_t>::max();
+class JobSystem2;
 
-    size_t index = InvalidIndex;
+struct JobHandle2
+{
+    IntrusiveFreeListIndex completion_index = IntrusiveFreeListInvalidIndex;
     size_t generation = 0;
+    JobSystem2* owner = nullptr;
 
-    bool is_valid() const { return index != InvalidIndex; }
+    bool is_valid() const { return completion_index != IntrusiveFreeListInvalidIndex; }
 };
 
-class InplaceJobFunction
-{
-  public:
-    InplaceJobFunction() = default;
-    InplaceJobFunction(InplaceJobFunction&& other) noexcept { move_from(std::move(other)); }
-
-    InplaceJobFunction& operator=(InplaceJobFunction&& other) noexcept
-    {
-        if (this != &other)
-        {
-            reset();
-            move_from(std::move(other));
-        }
-
-        return *this;
-    }
-
-    InplaceJobFunction(const InplaceJobFunction&) = delete;
-    InplaceJobFunction& operator=(const InplaceJobFunction&) = delete;
-    ~InplaceJobFunction() { reset(); }
-
-    template <typename Func>
-    static InplaceJobFunction create(Func&& func)
-    {
-        using StoredFuncT = std::decay_t<Func>;
-
-        static_assert(sizeof(StoredFuncT) <= InplaceJobMemoryBytes, "Template function too large for inplace job");
-        static_assert(
-            alignof(StoredFuncT) <= alignof(MemoryT), "Template function alignment too large for inplace job");
-
-        InplaceJobFunction result{};
-        new (&result.m_memory) StoredFuncT(std::forward<Func>(func));
-
-        result.m_invoke_func = [](void* memory) {
-            StoredFuncT& func = *reinterpret_cast<StoredFuncT*>(memory);
-            std::invoke(func);
-        };
-
-        result.m_destroy_func = [](void* memory) {
-            reinterpret_cast<StoredFuncT*>(memory)->~StoredFuncT();
-        };
-
-        result.m_move_func = [](void* src, void* dst) {
-            new (dst) StoredFuncT(std::move(*reinterpret_cast<StoredFuncT*>(src)));
-            reinterpret_cast<StoredFuncT*>(src)->~StoredFuncT();
-        };
-
-        return result;
-    }
-
-    void operator()()
-    {
-        MIZU_ASSERT(is_valid(), "Trying to execute invalid InplaceJobFunction");
-        m_invoke_func(&m_memory);
-    }
-
-    bool is_valid() const { return m_invoke_func != nullptr; }
-
-  private:
-    using MemoryT = std::aligned_storage_t<InplaceJobMemoryBytes, alignof(std::max_align_t)>;
-    MemoryT m_memory;
-
-    void (*m_invoke_func)(void*) = nullptr;
-    void (*m_destroy_func)(void*) = nullptr;
-    void (*m_move_func)(void*, void*) = nullptr;
-
-    void reset()
-    {
-        if (m_destroy_func != nullptr)
-        {
-            m_destroy_func(&m_memory);
-            m_invoke_func = nullptr;
-            m_destroy_func = nullptr;
-            m_move_func = nullptr;
-        }
-    }
-
-    void move_from(InplaceJobFunction&& other)
-    {
-        if (other.m_move_func != nullptr)
-        {
-            other.m_move_func(&other.m_memory, &m_memory);
-            m_invoke_func = other.m_invoke_func;
-            m_destroy_func = other.m_destroy_func;
-            m_move_func = other.m_move_func;
-
-            other.m_invoke_func = nullptr;
-            other.m_destroy_func = nullptr;
-            other.m_move_func = nullptr;
-        }
-    }
-};
-
-class Job2
+class JobDescription
 {
   public:
     template <typename Func, typename... Args>
-    static Job2 create(std::string_view name, Func&& func, Args&&... args)
+    static JobDescription create(Func&& func, Args&&... args)
     {
-        Job2 job{};
-        job.m_name = name;
-        job.m_func =
-            InplaceJobFunction::create([func = std::forward<Func>(func),
-                                        ... args = std::forward<Args>(args)]() mutable { std::invoke(func, args...); });
+        JobDescription desc{};
+        desc.m_func = InplaceJobFunction::create(func, args...);
 
-        return job;
+        return desc;
     }
 
-    Job2& depends_on(JobHandle handle)
+    JobDescription&& name(std::string_view new_name) &&
     {
-        MIZU_ASSERT(m_dependencies.size() < MaxJobDependencies, "Too many dependencies");
-        m_dependencies.push_back(handle);
-
-        return *this;
+        m_name = new_name;
+        return std::move(*this);
     }
 
-    Job2& set_affinity(JobAffinity affinity)
+    JobDescription&& affinity(JobAffinity affinity) &&
     {
         m_affinity = affinity;
+        return std::move(*this);
+    }
+
+  private:
+    InplaceJobFunction m_func{};
+    std::string_view m_name{};
+    JobAffinity m_affinity = JobAffinity::Any;
+
+    friend class PendingJob;
+    friend class JobSystem2;
+};
+
+class PendingJob
+{
+  public:
+    PendingJob(const PendingJob&) = delete;
+    PendingJob& operator=(const PendingJob&) = delete;
+    PendingJob(PendingJob&&) = delete;
+    PendingJob& operator=(PendingJob&&) = delete;
+
+    ~PendingJob()
+    {
+        MIZU_ASSERT(m_submitted, "The Job was not submitted, did you forget to call `schedule().submit()`?");
+    }
+
+    PendingJob& name(std::string_view name)
+    {
+        m_desc.m_name = name;
         return *this;
     }
 
-    void execute() { m_func(); }
+    PendingJob& affinity(JobAffinity affinity)
+    {
+        m_desc.m_affinity = affinity;
+        return *this;
+    }
 
-    bool has_dependencies() const { return !m_dependencies.empty(); }
+    PendingJob& depends_on(JobHandle2 handle)
+    {
+        m_dependencies.push_back(handle);
+        return *this;
+    }
+
+    JobHandle2 submit();
 
   private:
-    std::string_view m_name{};
+    JobSystem2* m_owner = nullptr;
 
-    InplaceJobFunction m_func{};
-    inplace_vector<JobHandle, MaxJobDependencies> m_dependencies{};
-    JobAffinity m_affinity = JobAffinity::None;
+    JobDescription m_desc{};
+    inplace_vector<JobHandle2, MaxJobDependencies> m_dependencies{};
+
+    bool m_submitted = false;
+
+    template <typename Func, typename... Args>
+    PendingJob(JobSystem2* owner, Func&& func, Args&&... args) : m_owner(owner)
+    {
+        m_desc = JobDescription{}.create(func, args...);
+    }
 
     friend class JobSystem2;
+};
+
+class PendingBatch
+{
+  public:
+    PendingBatch(const PendingBatch&) = delete;
+    PendingBatch& operator=(const PendingBatch&) = delete;
+    PendingBatch(PendingBatch&&) = delete;
+    PendingBatch& operator=(PendingBatch&&) = delete;
+
+    ~PendingBatch()
+    {
+        MIZU_ASSERT(m_submitted, "The Batch was not submitted, did you forget to call `schedule_batch().submit()`?");
+    }
+
+    PendingBatch& add(JobDescription&& desc)
+    {
+        m_jobs.push_back(std::move(desc));
+        return *this;
+    }
+
+    template <typename Func, typename... Args>
+    PendingBatch& add(Func&& func, Args&&... args)
+    {
+        return add(JobDescription::create(func, args...));
+    }
+
+    PendingBatch& depends_on(JobHandle2 handle)
+    {
+        m_dependencies.push_back(handle);
+        return *this;
+    }
+
+    JobHandle2 submit();
+
+  private:
+    JobSystem2* m_owner = nullptr;
+
+    static constexpr size_t MaxBatchJobs = 64;
+    inplace_vector<JobDescription, MaxBatchJobs> m_jobs{};
+    inplace_vector<JobHandle2, MaxJobDependencies> m_dependencies{};
+
+    bool m_submitted = false;
+
+    PendingBatch(JobSystem2* owner) : m_owner(owner) {}
+
+    friend class JobSystem2;
+};
+
+enum class JobState
+{
+    Free,
+    PendingDependencies,
+    Ready,
+    Running,
+    Waiting,
+    Finished,
+};
+
+enum class WaitNodeType
+{
+    DependencyEdge,
+    SuspendedTask,
+    CounterWait,
+};
+
+struct IntrusiveFreeListRecordBase
+{
+    IntrusiveFreeListIndex pool_index = IntrusiveFreeListInvalidIndex;
+    IntrusiveFreeListIndex next_free = IntrusiveFreeListInvalidIndex;
+};
+
+struct JobRecord : public IntrusiveFreeListRecordBase
+{
+    JobState state = JobState::Free;
+    InplaceJobFunction func{};
+
+    uint32_t num_remaining_dependencies = 0;
+    IntrusiveFreeListIndex completion_index = IntrusiveFreeListInvalidIndex;
+
+    // For suspension during execution (wait_for)
+    IntrusiveFreeListIndex waiting_on_completion_index = IntrusiveFreeListInvalidIndex;
+    IntrusiveFreeListIndex waiting_on_counter_index = IntrusiveFreeListInvalidIndex;
+};
+
+struct CompletionRecord : public IntrusiveFreeListRecordBase
+{
+    std::atomic<bool> is_completed = false;
+    // Number of jobs that need to finish before we consider this CompletionRecord completed.
+    std::atomic<uint32_t> counter = 0;
+
+    // -1 to differentiate with IntrusiveFreeListInvalidIndex. IntrusiveFreeListInvalidIndex means the list is empty.
+    static constexpr IntrusiveFreeListIndex ClosedWaitingList = std::numeric_limits<IntrusiveFreeListIndex>::max() - 1;
+    std::atomic<IntrusiveFreeListIndex> waiting_jobs_head = IntrusiveFreeListInvalidIndex;
+
+    std::atomic<uint32_t> references = 0;
+    uint32_t generation = 0;
+};
+
+struct WaitNode : public IntrusiveFreeListRecordBase
+{
+    WaitNodeType type{};
+    IntrusiveFreeListIndex target_job_index = IntrusiveFreeListInvalidIndex;
+    IntrusiveFreeListIndex next = IntrusiveFreeListInvalidIndex;
+
+    // For resuming jobs, prefer the same worker it was already being executed in for cache locality purposes.
+    // It's not a guarantee, just a hint.
+    uint32_t preferred_resume_worker_id = 0;
 };
 
 class MIZU_CORE_API JobSystem2
 {
   public:
-    JobSystem2();
-
     bool init(uint32_t num_workers);
 
-    void schedule();
+    template <typename Func, typename... Args>
+    PendingJob schedule(Func&& func, Args&&... args)
+    {
+        return PendingJob{this, std::forward<Func>(func), std::forward<Args>(args)...};
+    }
+
+    PendingBatch schedule_batch() { return PendingBatch{this}; }
 
   private:
+    static constexpr size_t WorkerQueueCapacity = 128;
+    static constexpr size_t PoolCapacity = 2048;
+
+    struct WorkerInfo
+    {
+        WorkStealingDeque<JobRecord, WorkerQueueCapacity> local_queue;
+    };
+
+    IntrusiveFreeList<JobRecord, PoolCapacity> m_job_record_pool;
+    IntrusiveFreeList<CompletionRecord, PoolCapacity> m_completion_record_pool;
+    IntrusiveFreeList<WaitNode, PoolCapacity> m_wait_node_pool;
+
     void worker_job();
+
+    JobHandle2 submit_internal(PendingJob&& job);
+    JobHandle2 submit_internal(PendingBatch&& batch);
+
+    void init_job_record(JobDescription& job, JobRecord& job_record, const CompletionRecord& completion_record);
+    void init_completion_record(CompletionRecord& completion_record, uint32_t counter);
+
+    CompletionRecord* try_get_job_handle_completion_record(const JobHandle2& handle);
+    CompletionRecord& get_job_handle_completion_record(const JobHandle2& handle);
+    bool try_push_wait_node(CompletionRecord& completion_record, WaitNode& wait_node);
+
+    JobRecord& allocate_job_record();
+    CompletionRecord& allocate_completion_record();
+    WaitNode& allocate_wait_node();
+
+    void free_wait_node(const WaitNode& wait_node);
+
+    friend class PendingJob;
+    friend class PendingBatch;
 };
 
 } // namespace Mizu
