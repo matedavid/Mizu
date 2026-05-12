7d9887423ccecf199af2422d724a42ab3747710c
David Mate <david.mate.lopez@gmail.com>
2026-05-12 02:08:06 +0200

    helper added

--- Engine/cmake/GamePackage.cmake
+++ Engine/cmake/GamePackage.cmake
@@ -31,6 +31,7 @@ function(mizu_add_game_package)
 
     # Create package-owned shader pipeline executable.
     mizu_configure_shader_pipeline(${MIZU_TARGET}.Pipeline)
+    set_target_properties(${MIZU_TARGET} PROPERTIES MIZU_GAME_PACKAGE_PIPELINE_TARGET "${MIZU_TARGET}.Pipeline")
 
     message(STATUS "Configured Game Package:")
     message(STATUS "Target       : ${MIZU_TARGET}")
@@ -38,4 +39,23 @@ function(mizu_add_game_package)
     message(STATUS "Assets Root  : ${MIZU_ASSETS_ROOT}")
     message(STATUS "")
 
+endfunction()
+
+function(mizu_game_package_attach_shader_module package_target module_target)
+    if (NOT TARGET ${package_target})
+        message(FATAL_ERROR "mizu_game_package_attach_shader_module: unknown package target '${package_target}'")
+    endif ()
+
+    if (NOT TARGET ${module_target})
+        message(FATAL_ERROR "mizu_game_package_attach_shader_module: unknown shader module target '${module_target}'")
+    endif ()
+
+    get_target_property(pipeline_target ${package_target} MIZU_GAME_PACKAGE_PIPELINE_TARGET)
+    if (NOT pipeline_target OR pipeline_target STREQUAL "pipeline_target-NOTFOUND")
+        message(FATAL_ERROR
+            "mizu_game_package_attach_shader_module: target '${package_target}' is not configured as a game package")
+    endif ()
+
+    target_link_libraries(${package_target} PUBLIC $<LINK_LIBRARY:WHOLE_ARCHIVE,${module_target}>)
+    mizu_attach_shader_module(${pipeline_target} ${module_target})
 endfunction()
